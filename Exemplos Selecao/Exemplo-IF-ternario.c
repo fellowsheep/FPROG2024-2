7dee4c2f8a6f9c03419cd9102bba3feeeb18ab50
Rossana Baptista Queiroz <fellowsheep@gmail.com>
2024-09-11 16:30:41 -0300

    Create Exemplo-IF-ternario.c

---
+++ Exemplos Selecao/Exemplo-IF-ternario.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10, b = 20;
+    int maior = (a > b) ? a : b; // Se a > b for verdadeira, "maior" recebe "a", caso contrário recebe "b"
+    printf("O maior valor é %d", maior);
+
+
+    return 0;
+}
+
+    
